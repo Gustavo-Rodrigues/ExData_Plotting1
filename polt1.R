d8169de60e6825edd17dc4d45addc027398314e6
Gustavo <uspgrs@gmail.com>
2017-01-05 17:11:27 -0200

    plot1

--- polt1.R
+++ polt1.R
@@ -1,6 +1,7 @@
 #rawData <- read.csv("household_power_consumption.txt",header = TRUE,sep = ";")
 dataset <- read.table("household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors=FALSE)
+plot1 <- subset(dataset, Date == "1/2/2007" | Date == "2/2/2007")
 png(filename = "plot1.png",width = 480, height = 480)
-hist(as.numeric(data$Global_active_power), col = "red", main = "Global Active Power", xlab = "Global Active Power(kilowatts)")
+hist(as.numeric(plot1$Global_active_power), col = "red", main = "Global Active Power", xlab = "Global Active Power(kilowatts)")
 dev.off()
 
