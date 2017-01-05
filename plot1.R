136c461331e180ae82a97a401a7a466a3ce069db
Gustavo <uspgrs@gmail.com>
2017-01-05 17:14:40 -0200

    plot1

--- plot1.R
+++ plot1.R
@@ -1,3 +1,4 @@
+#rawData <- read.csv("household_power_consumption.txt",header = TRUE,sep = ";")
 dataset <- read.table("household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors=FALSE)
 plot1 <- subset(dataset, Date == "1/2/2007" | Date == "2/2/2007")
 png(filename = "plot1.png",width = 480, height = 480)
